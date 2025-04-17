563b543bd7aaed49fc375435130e9bad06bec9da
LeeKM321 <stephen4951@gmail.com>
2025-04-17 11:47:13 +0900

    initial commit

---
+++ src/main/java/com/study/springcore/SpringCoreStudy202504Application.java
@@ -0,0 +1,13 @@
+package com.study.springcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCoreStudy202504Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCoreStudy202504Application.class, args);
+    }
+
+}
